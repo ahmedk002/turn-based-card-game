a5033caabd07188685a86ca348a8c545427f59a0
Riley Chen <rc047@bucknell.edu>
2025-04-16 14:56:57 -0400

    Worked on enums

--- src/main/java/cards/CardEffect.java
+++ src/main/java/cards/CardEffect.java
@@ -1,4 +1,48 @@
 package cards;
 
 public enum CardEffect {
+    DECREASE_LOW("D1"),
+    DECREASE_MED("D5"),
+    DECREASE_HIGH("D10"),
+    INCREASE_LOW("I1"),
+    INCREASE_MED("I5"),
+    INCREASE_HIGH("I10"),
+    HEAL_LOW("H1"),
+    HEAL_MED("H5"),
+    HEAL_HIGH("H10");
+
+    private final String effect;
+
+    private static final int LOW_STRENGTH = 1;
+    private static final int MEDIUM_STRENGTH = 5;
+    private static final int HIGH_STRENGTH = 10;
+
+    CardEffect(String effect) {
+        this.effect = effect;
+    }
+
+    public String toString() {
+        String description = switch (effect.charAt(0)) {
+            case 'D' -> "damage decreases";
+            case 'I' -> "damage increases";
+            case 'H' -> "heals";
+            default -> "";
+        } + getStrength();
+        return description;
+    }
+
+    public String getStrength() {
+        int strength = Integer.parseInt(effect.substring(1));
+        if (strength == LOW_STRENGTH) {
+            return " slightly";
+        } else if (strength == MEDIUM_STRENGTH) {
+            return "";
+        } else {
+            return " significantly";
+        }
+    }
+
+    public int getEffect() {
+        return Integer.parseInt(effect.substring(1));
+    }
 }
--- src/main/java/cards/CardType.java
+++ src/main/java/cards/CardType.java
@@ -10,4 +10,14 @@ public enum CardType {
     CardType(char designator) {
         this.designator = designator;
     }
+
+    public String toString() {
+        String type = switch (designator) {
+            case 's' -> "status";
+            case 'a' -> "attack";
+            case 'c' -> "creature";
+            default -> "";
+        };
+        return type;
+    }
 }
