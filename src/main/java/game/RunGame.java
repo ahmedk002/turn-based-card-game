0a00a3200538789b18d3cb044a3935cbef565332
Nathan Ramkissoon <njr015@bucknell.edu>
2025-04-22 19:56:00 -0400

    RunGame setup

--- src/main/java/game/RunGame.java
+++ src/main/java/game/RunGame.java
@@ -19,9 +19,13 @@ import cards.*;
 import creature.*;
 
 public class RunGame {
-    Player gamer = new Player(50);
-    Creature goblin = new Creature("Goblin", 5, 300);
+    public static void main(String[] args) {
+        Player gamer = new Player(50);
+        Creature goblin = new Creature("Goblin", 5, 300);
 
-    Card fireball = new AttackCard("Fire Ball", null, 10);
+        Card fireball = new AttackCard("Fire Ball", null, 10);
 
+        BattleManager battle = new BattleManager(gamer, goblin);
+        battle.battleLoop();
+    }
 }
